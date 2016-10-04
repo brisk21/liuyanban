1baa55929814edd5c6f0946a3757028a96d86ff3
Brisk Ver <1076963452@qq.com>
2016-10-04 11:16:04 +0800

    配置修改

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Shanghai');//时区
 //定义数据库信息
 $dbhost = 'localhost';
 $dbuser = 'root';
-$dbpasswd = 'wbs123';
+$dbpasswd = 'root';
 $dbname = 'php10';
 //定义每页显示量
 $pageTotal = 5;
